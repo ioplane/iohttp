f6b4cebd9eee2db0382700da17b4924ee402e892
Pavel Lavrukhin <46395539+dantte-lp@users.noreply.github.com>
2026-03-08 19:31:51 +0300

    test(quic): add QUIC connection handshake tests with real certs (3 tests)

--- tests/unit/test_io_quic.c
+++ tests/unit/test_io_quic.c
@@ -1,6 +1,8 @@
 #include <unity/unity.h>
 #include "http/io_quic.h"
+#include <arpa/inet.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <string.h>
 
 void setUp(void) {}
@@ -125,6 +127,146 @@ void test_quic_write_stream_null(void)
     TEST_ASSERT_TRUE(io_quic_write_stream(nullptr, 0, data, 5, false) < 0);
 }
 
+/* ---- Handshake tests (require test certs) ---- */
+
+static io_quic_config_t test_server_config(void)
+{
+    io_quic_config_t cfg;
+    io_quic_config_init(&cfg);
+    cfg.cert_file = TEST_CERTS_DIR "/server.crt";
+    cfg.key_file = TEST_CERTS_DIR "/server.key";
+    return cfg;
+}
+
+/* Track callback invocations */
+static bool g_handshake_done;
+static int g_streams_opened;
+static int g_data_received;
+static uint8_t g_received_data[4096];
+static size_t g_received_len;
+
+static void reset_globals(void)
+{
+    g_handshake_done = false;
+    g_streams_opened = 0;
+    g_data_received = 0;
+    g_received_len = 0;
+    memset(g_received_data, 0, sizeof(g_received_data));
+}
+
+static void on_handshake_done(io_quic_conn_t *conn, void *user_data)
+{
+    (void)conn;
+    (void)user_data;
+    g_handshake_done = true;
+}
+
+static int on_stream_open(io_quic_conn_t *conn, int64_t stream_id, void *user_data)
+{
+    (void)conn;
+    (void)stream_id;
+    (void)user_data;
+    g_streams_opened++;
+    return 0;
+}
+
+static int on_stream_data(io_quic_conn_t *conn, int64_t stream_id,
+                           const uint8_t *data, size_t len,
+                           bool fin, void *user_data)
+{
+    (void)conn;
+    (void)stream_id;
+    (void)fin;
+    (void)user_data;
+    g_data_received++;
+    if (g_received_len + len <= sizeof(g_received_data)) {
+        memcpy(g_received_data + g_received_len, data, len);
+        g_received_len += len;
+    }
+    return 0;
+}
+
+void test_quic_conn_create_with_certs(void)
+{
+    reset_globals();
+    io_quic_config_t cfg = test_server_config();
+    io_quic_callbacks_t cbs = {
+        .on_stream_data = on_stream_data,
+        .on_stream_open = on_stream_open,
+        .on_handshake_done = on_handshake_done,
+    };
+    uint8_t dcid[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    uint8_t scid[8] = {9, 10, 11, 12, 13, 14, 15, 16};
+    struct sockaddr_in local = {.sin_family = AF_INET, .sin_port = htons(443)};
+    struct sockaddr_in remote = {.sin_family = AF_INET, .sin_port = htons(12345)};
+
+    io_quic_conn_t *conn = io_quic_conn_create(&cfg, &cbs, dcid, 8, scid, 8,
+                                                 (struct sockaddr *)&local,
+                                                 (struct sockaddr *)&remote,
+                                                 nullptr);
+    /* Connection creation may fail if cert files don't exist */
+    if (conn == nullptr) {
+        TEST_IGNORE_MESSAGE("Could not create QUIC conn (cert files missing?)");
+        return;
+    }
+    TEST_ASSERT_FALSE(io_quic_is_handshake_done(conn));
+    TEST_ASSERT_FALSE(io_quic_is_closed(conn));
+
+    /* Server should have initial data to send (crypto frames) */
+    const uint8_t *out;
+    size_t out_len;
+    int rc = io_quic_flush(conn, &out, &out_len);
+    TEST_ASSERT_EQUAL_INT(0, rc);
+
+    io_quic_conn_destroy(conn);
+}
+
+void test_quic_get_timeout_not_max_after_create(void)
+{
+    io_quic_config_t cfg = test_server_config();
+    io_quic_callbacks_t cbs = {0};
+    uint8_t dcid[8] = {1};
+    uint8_t scid[8] = {2};
+    struct sockaddr_in local = {.sin_family = AF_INET, .sin_port = htons(443)};
+    struct sockaddr_in remote = {.sin_family = AF_INET, .sin_port = htons(12345)};
+
+    io_quic_conn_t *conn = io_quic_conn_create(&cfg, &cbs, dcid, 8, scid, 8,
+                                                 (struct sockaddr *)&local,
+                                                 (struct sockaddr *)&remote,
+                                                 nullptr);
+    if (conn == nullptr) {
+        TEST_IGNORE_MESSAGE("cert files missing");
+        return;
+    }
+    /* After creation, there should be a timeout (handshake timers) */
+    uint64_t timeout = io_quic_get_timeout(conn);
+    TEST_ASSERT_TRUE(timeout < UINT64_MAX);
+    io_quic_conn_destroy(conn);
+}
+
+void test_quic_close_sets_closed(void)
+{
+    io_quic_config_t cfg = test_server_config();
+    io_quic_callbacks_t cbs = {0};
+    uint8_t dcid[8] = {1};
+    uint8_t scid[8] = {2};
+    struct sockaddr_in local = {.sin_family = AF_INET, .sin_port = htons(443)};
+    struct sockaddr_in remote = {.sin_family = AF_INET, .sin_port = htons(12345)};
+
+    io_quic_conn_t *conn = io_quic_conn_create(&cfg, &cbs, dcid, 8, scid, 8,
+                                                 (struct sockaddr *)&local,
+                                                 (struct sockaddr *)&remote,
+                                                 nullptr);
+    if (conn == nullptr) {
+        TEST_IGNORE_MESSAGE("cert files missing");
+        return;
+    }
+    int rc = io_quic_close(conn, 0);
+    TEST_ASSERT_EQUAL_INT(0, rc);
+    TEST_ASSERT_TRUE(io_quic_is_closed(conn));
+    io_quic_conn_destroy(conn);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
@@ -145,5 +287,9 @@ int main(void)
     RUN_TEST(test_quic_state_queries_null);
     RUN_TEST(test_quic_close_null);
     RUN_TEST(test_quic_write_stream_null);
+    /* Handshake (require test certs) */
+    RUN_TEST(test_quic_conn_create_with_certs);
+    RUN_TEST(test_quic_get_timeout_not_max_after_create);
+    RUN_TEST(test_quic_close_sets_closed);
     return UNITY_END();
 }
